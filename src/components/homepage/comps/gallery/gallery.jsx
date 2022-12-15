6350a2dab5454e418a02394cb4e83ff7708a981f
anaahmedgohary <gogoahmed13@gmail.com>
2022-12-15 03:01:22 +0200

    secondcommit

--- src/components/homepage/comps/gallery/gallery.jsx
+++ src/components/homepage/comps/gallery/gallery.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import "./gallery.css";
 
 // images
-import img01 from "/homeimages/1535446.jpg";
-import img02 from "/homeimages/timeline02.jpg";
-import img03 from "/homeimages/femalebb-1.png";
-import img04 from "/homeimages/femalebb-16.jpg";
-import img05 from "/homeimages/1664275511.png";
+import img01 from "/homeimages/headerbg-2.jpg";
+import img05 from "/homeimages/headerbg-3.jpg";
 
-import img06 from "/homeimages/femalebb-4.jpg";
-import img07 from "/homeimages/bodybuilding-tips.jpg";
-import img08 from "/homeimages/qqqqqww.jpg";
+import img06 from "/homeimages/headerbg-1.jpeg";
+import img07 from "/homeimages/Gym-Equipment.jpg";
 
 export default function GalleryComp()
 {
@@ -31,18 +27,18 @@ export default function GalleryComp()
 
       <div className='trainorsGallery'>
         <div className='trainorP'>
-          <img id='mainImg' className='sideImg' src={img05} alt="" />
+          <img id='mainImg' className='sideImg' src={img01} alt="" />
         </div>
 
         <div className='imgsGallery'>
           <div className='imgcol'>
+            <img className='GalleryImg' src={img01} alt="firstpic" onClick={chooseImg} />
             <img className='GalleryImg' src={img05} alt="firstpic" onClick={chooseImg} />
-            <img className='GalleryImg' src={img07} alt="firstpic" onClick={chooseImg} />
           </div>
 
           <div className='imgcol'>
             <img className='GalleryImg' src={img06} alt="firstpic" onClick={chooseImg} />
-            <img className='GalleryImg' src={img08} alt="firstpic" onClick={chooseImg} />
+            <img className='GalleryImg' src={img07} alt="firstpic" onClick={chooseImg} />
           </div>
         </div>
       </div>
